eb67ab5958d1775d27256500d49bf6c14cefb906
Thanasis Mattas <atmattas@physics.auth.gr>
2019-08-13 11:25:30 +0300

    comment out delete_logs_dats_images()

--- mattflow/__main__.py
+++ mattflow/__main__.py
@@ -49,7 +49,7 @@ def main():
     # }
 
     # Uncomment this to delete previous log, dat and png files (for debugging)
-    util.delete_logs_dats_images()
+    # util.delete_logs_dats_images()
 
     # Solution {
     #
