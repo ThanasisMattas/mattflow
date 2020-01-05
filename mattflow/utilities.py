69800f5da706f5306666689211d701a391023f15
Thanasis Mattas <atmattas@physics.auth.gr>
2020-01-05 11:33:54 +0100

    doc: edit module discription

--- mattflow/utilities.py
+++ mattflow/utilities.py
@@ -1,5 +1,4 @@
 '''
-=============================================================================
 @file   utilities.py  
 @author Thanasis Mattas
 
@@ -10,7 +9,6 @@ terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later
 version. You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
-=============================================================================
 '''
 
 
